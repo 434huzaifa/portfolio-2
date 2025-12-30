0d9d1c58ec36ba4cfdf771504350b70235b4013f
Md. Huzaifa <saadhuzaifa2497@gmail.com>
2025-12-31 04:58:25 +0600

    Add accessibility panel with font size controls on left side

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import Awards from '@/components/Awards';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
 import AnimatedBackground from '@/components/AnimatedBackground';
+import AccessibilityPanel from '@/components/AccessibilityPanel';
 import { ThemeProvider } from '@/contexts/ThemeContext';
 import portfolioData from '@/portfolio-data.json';
 import { validatePortfolioData } from '@/lib/portfolio-utils';
@@ -78,6 +79,7 @@ export default function Home() {
   return (
     <ThemeProvider>
       <AnimatedBackground />
+      <AccessibilityPanel />
       <Navigation />
       <main>
         {data.sectionOrder.map((section: string) => {
